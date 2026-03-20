17c467155628bf56c6cf6fc6adef89e7b0e1607f
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-20 10:51:56 +0530

    updated EnvelopedEncodable optimization

--- src/enveloped.rs
+++ src/enveloped.rs
@@ -1,6 +1,5 @@
 use bytes::BytesMut;
 
-/// DecoderError for typed transactions.
 #[derive(Clone, Debug, Eq, PartialEq)]
 pub enum EnvelopedDecoderError<T> {
 	UnknownTypeId,
@@ -13,34 +12,29 @@ impl<T> From<T> for EnvelopedDecoderError<T> {
 	}
 }
 
-/// Encodable typed transactions.
 pub trait EnvelopedEncodable {
-	/// Convert self to an owned vector.
 	fn encode(&self) -> BytesMut {
 		let type_id = self.type_id();
+		let payload = self.encode_payload();
 
-		let mut out = BytesMut::new();
-		if let Some(type_id) = type_id {
-			assert!(type_id <= 0x7f);
-			out.extend_from_slice(&[type_id]);
+		let mut out = BytesMut::with_capacity(payload.len() + 1);
+
+		if let Some(id) = type_id {
+			assert!(id <= 0x7f);
+			out.extend_from_slice(&[id]);
 		}
 
-		out.extend_from_slice(&self.encode_payload()[..]);
+		out.extend_from_slice(&payload);
 		out
 	}
 
-	/// Type Id of the transaction.
 	fn type_id(&self) -> Option<u8>;
 
-	/// Encode inner payload.
 	fn encode_payload(&self) -> BytesMut;
 }
 
-/// Decodable typed transactions.
 pub trait EnvelopedDecodable: Sized {
-	/// Inner payload decoder error.
 	type PayloadDecoderError;
 
-	/// Decode raw bytes to a Self type.
 	fn decode(bytes: &[u8]) -> Result<Self, EnvelopedDecoderError<Self::PayloadDecoderError>>;
 }
