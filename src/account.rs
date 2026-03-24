dd88d2ed15ac51313dab0ab70b748271545034cb
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-24 10:19:48 +0530

    refactor Account struct (short field names)

--- src/account.rs
+++ src/account.rs
@@ -3,13 +3,13 @@ use ethereum_types::{H256, U256};
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[derive(rlp::RlpEncodable, rlp::RlpDecodable)]
 #[cfg_attr(
-	feature = "with-scale",
-	derive(scale_codec::Encode, scale_codec::Decode, scale_info::TypeInfo)
+    feature = "with-scale",
+    derive(scale_codec::Encode, scale_codec::Decode, scale_info::TypeInfo)
 )]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Account {
-	pub nonce: U256,
-	pub balance: U256,
-	pub storage_root: H256,
-	pub code_hash: H256,
+    pub bal: U256,
+    pub nonce: U256,
+    pub sroot: H256,
+    pub chash: H256,
 }
