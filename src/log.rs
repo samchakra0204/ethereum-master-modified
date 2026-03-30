a9f3a4255e2a64268b85c4426243f376e1f70195
Samag Chakrabarti <samagchakrabarti@gmail.com>
2026-03-30 10:09:25 +0530

    Update Log struct with constructor and helper

--- src/log.rs
+++ src/log.rs
@@ -4,20 +4,30 @@ use ethereum_types::{H160, H256};
 
 use crate::Bytes;
 
-#[derive(Clone, Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 #[derive(rlp::RlpEncodable, rlp::RlpDecodable)]
 #[cfg_attr(
-	feature = "with-scale",
-	derive(
-		scale_codec::Encode,
-		scale_codec::Decode,
-		scale_codec::DecodeWithMemTracking,
-		scale_info::TypeInfo
-	)
+    feature = "with-scale",
+    derive(
+        scale_codec::Encode,
+        scale_codec::Decode,
+        scale_codec::DecodeWithMemTracking,
+        scale_info::TypeInfo
+    )
 )]
 #[cfg_attr(feature = "with-serde", derive(serde::Serialize, serde::Deserialize))]
 pub struct Log {
-	pub address: H160,
-	pub topics: Vec<H256>,
-	pub data: Bytes,
+    pub address: H160,
+    pub topics: Vec<H256>,
+    pub data: Bytes,
+}
+
+impl Log {
+    pub fn new(address: H160, topics: Vec<H256>, data: Bytes) -> Self {
+        Self { address, topics, data }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.topics.is_empty() && self.data.is_empty()
+    }
 }
